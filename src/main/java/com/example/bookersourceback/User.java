4851ee42d7590f9caa2597f7853612fa71f13669
alexandreSilva604 <xandlibra604@gmail.com>
2024-11-25 09:56:40 -0300

    Created User controller and repository, and added attributes to User class.

--- src/main/java/com/example/bookersourceback/User.java
+++ src/main/java/com/example/bookersourceback/User.java
@@ -12,14 +12,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private int zipCode;
-    private String name, email, password;
+    private String name, email, password, country, state, city, address;
     private boolean isAdministrator;
 
-    public User(String name, String email, String password, int zipCode, boolean isAdministrator) {
+    public User(String name, String email, String password,
+                String country, String state, String city, String address,
+                int zipCode, boolean isAdministrator) {
 
         this.name = name;
         this.email = email;
         this.password = password;
+        this.country = country;
+        this.city = city;
+        this.address = address;
         this.zipCode = zipCode;
         this.isAdministrator = isAdministrator;
     }
@@ -34,6 +39,18 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getAddress() {
+        return address;
     }
 
     public int getZipCode() {
---
+++ src/main/java/com/example/bookersourceback/UserController.java
@@ -0,0 +1,35 @@
+package com.example.bookersourceback;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path="/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser(@RequestParam String name,
+                                            @RequestParam String email,
+                                            @RequestParam String password,
+                                            @RequestParam String country,
+                                            @RequestParam String state,
+                                            @RequestParam String city,
+                                            @RequestParam String address,
+                                            @RequestParam int zipCode,
+                                            @RequestParam boolean isAdministrator) {
+
+        User newUser = new User(name, email, password, country, state,
+                city, address, zipCode, isAdministrator);
+        userRepository.save(newUser);
+
+        return "Added to repository";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
