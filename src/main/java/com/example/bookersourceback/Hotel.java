53afcdd155ffa5a6af1f704686fdf5e053567b8d
alexandreSilva604 <xandlibra604@gmail.com>
2025-01-27 14:20:43 -0300

    Hotel.java attributes now match to the ones in the database.

--- src/main/java/com/example/bookersourceback/Hotel.java
+++ src/main/java/com/example/bookersourceback/Hotel.java
@@ -1,28 +1,44 @@
 package com.example.bookersourceback;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name="hotels", schema="mydatabase")
 public class Hotel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
-    private int zipCode;
-    private String name, country, state, city, address;
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="hotel_id")
+    private long id;
 
-    public Hotel(String name, String country, String state, String city, String address, int zipCode) {
+    @Column(name="hotel_name")
+    private String name;
+
+    @Column(name="hotel_country")
+    private String country;
+
+    @Column(name="hotel_state")
+    private String state;
+
+    @Column(name="hotel_city")
+    private String city;
+
+    @Column(name="hotel_address")
+    private String address;
+
+    @Column(name="hotel_admin_id")
+    private long adminId;
+
+    public Hotel(String name, String country, String state, String city, String address, long adminId) {
         this.name = name;
         this.country = country;
         this.state = state;
         this.city = city;
         this.address = address;
+        this.adminId = adminId;
     }
 
-    public Integer getId() {
+    public long getId() {
         return this.id;
     }
 
@@ -46,7 +62,7 @@ public class Hotel {
         return this.address;
     }
 
-    public int getZipCode() {
-        return this.zipCode;
+    public long getAdminId() {
+        return adminId;
     }
 }
